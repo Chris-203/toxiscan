b03f5e1fa671023a4418804cbc3db177650ee07e
Manahil2026 <manahil2004@gmail.com>
2024-08-27 19:55:51 -0400

    add background image

--- toxiscan-app/app/page.js
+++ toxiscan-app/app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { Button } from "@mui/material";
+import { Button, Box } from "@mui/material";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
@@ -7,9 +7,16 @@ import { useRouter } from "next/navigation";
 export default function Home() {
     const router = useRouter();
     return (
-        <div>
-            <Button onClick={() => router.push('/scanner')}>Button</Button>
-        </div>
+        <Box sx={{
+          backgroundImage: "url('/images/background.png')",
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center center',
+          height: '100vh',
+          width: '100%',
+       }}>
+          
+        </Box>
     );
   
 }
