91ff211e15ee79c4e0111538701b8fd9a794f7bf
Manahil2026 <manahil2004@gmail.com>
2024-08-27 20:23:57 -0400

    add headings and button

--- toxiscan-app/app/page.js
+++ toxiscan-app/app/page.js
@@ -1,12 +1,15 @@
 'use client'
-import { Button, Box } from "@mui/material";
+import { Button, Box, Typography, ThemeProvider, useTheme } from "@mui/material";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import CustomTheme from './components/Theme';
 
 
 export default function Home() {
+    const theme = useTheme(); // Access theme using useTheme hook
     const router = useRouter();
     return (
+      <ThemeProvider theme={CustomTheme}>
         <Box sx={{
           backgroundImage: "url('/images/background.png')",
           backgroundSize: 'cover',
@@ -14,9 +17,31 @@ export default function Home() {
           backgroundPosition: 'center center',
           height: '100vh',
           width: '100%',
+          textAlign: "center",
+          paddingTop: '20vh', // Adjust the padding to move content down
        }}>
+                
+        
+          <Typography 
+          variant="h1" component="h1" gutterBottom 
+          color={"common.white"}>
+            <strong>Toxiscan</strong>
+          </Typography>
+          <Typography variant="h5" component="h2" gutterBottom color="common.white">
+          Your ultimate Guide to Healthy and Eco-Friendly Living
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ mt: 2, mr: 2 }}
+            href="/generate"
+          >
+            Start Scanning
+          </Button>
+        
           
         </Box>
+        </ThemeProvider>
     );
   
 }
