e75707773751128708ef6515c8255612b6dde68a
Manahil2026 <manahil2004@gmail.com>
2024-08-27 20:25:39 -0400

    add theme

---
+++ toxiscan-app/app/components/Theme.js
@@ -0,0 +1,18 @@
+'use client'
+import {createTheme} from '@mui/material/styles';
+
+
+
+    const CustomTheme = createTheme({
+        palette:{
+            primary:{
+                main:'#29524a',
+                
+        }
+        }
+
+    })
+
+
+
+export default CustomTheme;
