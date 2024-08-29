00bed67d77f27b8bff9bc0ad5c559e7c76835e12
Manahil2026 <manahil2004@gmail.com>
2024-08-28 21:10:40 -0400

    add contact us page

---
+++ toxiscan-app/app/contact/page.js
@@ -0,0 +1,11 @@
+'use client'
+import { Button, Box, Typography, ThemeProvider, useTheme, Toolbar } from "@mui/material";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import CustomTheme from './components/Theme';
+import CustomAppBar from './components/CustomAppBar'; 
+
+
+export default function contactPage() {
+    // Add your code for the default page here
+}
