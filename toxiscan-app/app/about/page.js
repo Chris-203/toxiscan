ab57b0ec4d8388665c6a35de50a25cab268a8a80
Manahil2026 <manahil2004@gmail.com>
2024-08-30 13:45:55 -0400

    change photo background to transparent

--- toxiscan-app/app/about/page.js
+++ toxiscan-app/app/about/page.js
@@ -1,12 +1,5 @@
-// Import any necessary modules or libraries here
-
-// Define your default page function
-
-    // Add your code for the default page here
-
-
 'use client';
-import { Box, Typography, Toolbar, ThemeProvider, useTheme} from "@mui/material";
+import { Box, Typography, Toolbar, ThemeProvider, useTheme, GlobalStyles} from "@mui/material";
 import Image from "next/image";
 import CustomAppBar from '../components/CustomAppBar'; 
 import CustomTheme from '../components/Theme'; 
@@ -16,6 +9,14 @@ export default function AboutUs() {
 
   return (
     <ThemeProvider theme={CustomTheme}>
+                  {/* Global Styles to set the background color of the whole screen */}
+                  <GlobalStyles
+        styles={{
+          body: {
+            backgroundColor: '#94a187', // Set your desired background color here
+          },
+        }}
+      />
       <CustomAppBar /> {/* Custom AppBar at the top */}
       <Toolbar/> {/* Toolbar to create space for the AppBar */}
       <Box
@@ -50,13 +51,13 @@ export default function AboutUs() {
               maxWidth: { xs: '100%', sm: '50%' },
             }}
           >
-            <Typography variant="body1" sx={{ fontWeight: 'bold', marginBottom: 2 }}>
+            <Typography variant="h4" sx={{ fontWeight: 'bold', marginBottom: 2 }}>
               Welcome to our Toxiscan app!
             </Typography>
-            <Typography variant="body1" sx={{ marginBottom: 2 }}>
+            <Typography variant="body1" sx={{ fontSize: '1.25rem', marginBottom: 2 }}>
               We are a team of three passionate and dedicated software engineering students united by our passion for technology and innovation. Our mission is to revolutionize food safety and chemical understanding through technology, so we created this Toxiscan app. Through this app, our goal is to develop cutting-edge solutions that address real-world challenges and make a meaningful impact.
             </Typography>
-            <Typography variant="body1">
+            <Typography variant="body1" sx={{ fontSize: '1.25rem' }}>
               The Toxiscan app is designed to empower users with crucial information about the chemicals in their food. By offering an intuitive and user-friendly platform, we aim to help people make informed decisions about their diet and health. Our app enables users to easily scan their food and understand the presence of potentially harmful chemicals, promoting a safer and more conscious approach to food consumption. Sign into Toxiscan today to start exploring the chemicals in your food and make smarter, healthier choices. Thank you for joining us on this journey toward safer and healthier living!
             </Typography>
           </Box>
@@ -66,15 +67,17 @@ export default function AboutUs() {
               flex: 1,
               textAlign: 'center',
               maxWidth: { xs: '100%', sm: '50%' },
+              marginLeft: { xs: 0, sm: 'auto' }, // Adjust marginLeft to position the image
+              transform: { xs: 'none', sm: 'translateX(50px)' }, // Optional: Use transform to nudge image further to the right
             }}
+          
           >
             <Image
-              src="/images/Toxiscanlogo.jpg"
+              src="/images/Toxiscanlogo.png"
               alt="About Us"
               layout="responsive"
               width={600}
               height={400}
-              style={{ borderRadius: '8px' }}
             />
           </Box>
         </Box>
