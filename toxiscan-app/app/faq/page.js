faaacb87c89d51ad39033122d63c2137f2a12c04
Syeda Begum <begum.syeda2003@gmail.com>
2024-08-30 02:17:53 -0400

    added faq

--- toxiscan-app/app/faq/page.js
+++ toxiscan-app/app/faq/page.js
@@ -1,49 +1,71 @@
-'use client'
+'use client';
 import React from 'react';
-import { Typography, Accordion, AccordionSummary, AccordionDetails, Container, Box } from '@mui/material';
+import { Typography, Accordion, AccordionSummary, AccordionDetails, Container, Box, Toolbar, ThemeProvider, useTheme } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import CustomAppBar from '../components/CustomAppBar';
+import CustomTheme from '../components/Theme';
 
 const FAQ = () => {
+  const theme = useTheme(); // Access theme using useTheme hook
+
   const faqs = [
     {
       question: "What is this app about?",
-      answer: "This app helps .",
+      answer: "This app allows you to scan food items to get a detailed list of ingredients and determine if any of them are toxic.",
     },
     {
       question: "How do I create an account?",
-      answer: "To create an account .",
+      answer: "To create an account, click on the 'Sign Up' button and follow the instructions.",
     },
     {
-      question: "How do I scan iteams ?",
-      answer: "You can scan iteams on the search bar.",
+      question: "How do I use the app to scan food items?",
+      answer: "You can scan items using the scan button on the search bar.Simply open the app, use the scan feature to capture the barcode of the food packaging, and the app will display the ingredients list along with any potential toxic substances.",
     },
     {
-        question: "How do I scan iteams ?",
-        answer: "You can scan iteams on the search bar.",
-      },
-   
+      question: "Can I use the app for foods without barcodes or labels?",
+      answer: "Currently, the app is optimized for scanning packaged foods with barcodes. For unlabeled or homemade foods, the app may not provide ingredients information.",
+    },
+    {
+      question: " Is the app accurate in identifying toxic ingredients?",
+      answer: "The app is designed to provide accurate information based on a comprehensive and research-backed database. However, it's always a good idea to verify with additional sources if you have specific concerns about certain ingredients.",
+    },
+
+    {
+      question: "How do I contact support?",
+      answer: "You can contact support by emailing your name, email, and message using the contact form on the website and our team will get back to as soon as possible.",
+    },
+    
   ];
 
   return (
-    <Container sx={{ mt: 8, mb: 4 }}>
-      <Box sx={{ textAlign: 'center', mb: 4 }}>
-        <Typography variant="h4" gutterBottom>
-          Frequently Asked Questions
-        </Typography>
-      </Box>
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        {faqs.map((faq, index) => (
-          <Accordion key={index} sx={{ width: '100%', maxWidth: 600, mb: 2 }}>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+    <ThemeProvider theme={CustomTheme}>
+      <CustomAppBar />
+      <Toolbar /> {/* Ensure enough space for the AppBar */}
+      <Container sx={{ mt: 8, mb: 4 }}>
+        <Box sx={{ textAlign: 'center', mb: 4 }}>
+          <Typography variant="h4" gutterBottom sx={{ fontWeight: 'bold' }}>
+            Frequently Asked Questions
+          </Typography>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          {faqs.map((faq, index) => (
+            <Accordion key={index} sx={{ width: '100%', maxWidth: 600, mb: 2 }}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}
+              sx={{ bgcolor: '#29524a', 
+                color: 'white',
+                '& .MuiAccordionSummary-content': { color: 'white'}
+                }}
+                >
               <Typography variant="h6">{faq.question}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>{faq.answer}</Typography>
-            </AccordionDetails>
-          </Accordion>
-        ))}
-      </Box>
-    </Container>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>{faq.answer}</Typography>
+              </AccordionDetails>
+            </Accordion>
+          ))}
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 };
 
