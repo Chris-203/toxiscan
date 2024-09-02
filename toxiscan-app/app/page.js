a3964157c2b213217327f35aea4e0d6097d67080
Manahil2026 <manahil2004@gmail.com>
2024-09-01 22:45:45 -0400

    add footer

--- toxiscan-app/app/page.js
+++ toxiscan-app/app/page.js
@@ -6,12 +6,14 @@ import {
   ThemeProvider,
   useTheme,
   Toolbar,
+  Divider,
+  IconButton,
 } from "@mui/material";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import CustomTheme from "./components/Theme";
 import CustomAppBar from "./components/CustomAppBar";
 import { useAuth } from "@clerk/nextjs";
+import { FaYoutube, FaInstagram, FaTiktok } from 'react-icons/fa';
 
 export default function Home() {
   const theme = useTheme(); // Access theme using useTheme hook
@@ -26,57 +28,118 @@ export default function Home() {
     }
   };
 
-
   return (
     <ThemeProvider theme={CustomTheme}>
       <CustomAppBar />
       <Toolbar /> {/* Add Toolbar to create space for the AppBar */}
+      
+      {/* Main Container with Background Image */}
       <Box
         sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh", // Ensure the container takes up at least the full height of the viewport
           backgroundImage: "url('/images/gradient2.png')",
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center center",
-          minHeight: "100vh", // Changed from height to minHeight
-          width: "100%",
-          textAlign: "center",
-          display: "flex", // Added flexbox to center content vertically
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          overflow: "hidden", // Prevents scrolling issues when zoomed in
+          paddingTop: '64px', // Adjust this value according to the height of your AppBar
         }}
       >
-        <Typography variant="h1" component="h1" gutterBottom color={"black"}>
-          <strong>Toxiscan</strong>
-        </Typography>
-        <Typography variant="h4" component="h2" gutterBottom color="black">
-          <strong>
-            Your ultimate Guide to Healthy and Eco-Friendly Living
-          </strong>
-        </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          sx={{ mt: 10, mr: 2, borderRadius: 10 }}
-          onClick={handleScanClick} // Use onClick to handle scanning
-          size="large"
-          style={{
-            fontSize: "25px",
+        <Box
+          sx={{
+            flexGrow: 1, // Expand this area to push the footer to the bottom
+            textAlign: "center",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            overflow: "hidden",
           }}
         >
-          <strong>Start Scanning</strong>
-        </Button>
+          <Typography variant="h1" component="h1" gutterBottom color={"black"}>
+            <strong>Toxiscan</strong>
+          </Typography>
+          <Typography variant="h4" component="h2" gutterBottom color="black">
+            <strong>
+              Your ultimate Guide to Healthy and Eco-Friendly Living
+            </strong>
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ mt: 10, mr: 2, borderRadius: 10 }}
+            onClick={handleScanClick} // Use onClick to handle scanning
+            size="large"
+            style={{
+              fontSize: "25px",
+            }}
+          >
+            <strong>Start Scanning</strong>
+          </Button>
 
-        <Typography
-          variant="h3"
-          component="h2"
-          gutterBottom
-          color="black"
-          paddingTop={10}
+          <Typography
+            variant="h3"
+            component="h2"
+            gutterBottom
+            color="black"
+            paddingTop={10}
+          >
+            <strong>Scan.Search.Know</strong>
+          </Typography>
+        </Box>
+
+        <Divider sx={{ mt: 10 }} /> {/* Divider with top margin */}
+
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "20px",
+            backgroundColor: '#9fdf9c',
+            borderTop: `1px solid ${theme.palette.divider}`,
+          }}
         >
-          <strong>Scan.Search.Know</strong>
-        </Typography>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            align="left"
+          >
+            © 2024 Toxiscan, all rights reserved
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              gap: 2, // Add some space between icons
+            }}
+          >
+            <IconButton
+              color="primary"
+              href="https://www.youtube.com/@Toxiscan"
+              target="_blank"
+              rel="noopener"
+            >
+              <FaYoutube size={24} />
+            </IconButton>
+            <IconButton
+              color="primary"
+              href="https://www.instagram.com/toxiscan4"
+              target="_blank"
+              rel="noopener"
+            >
+              <FaInstagram size={24} />
+            </IconButton>
+            <IconButton
+              color="primary"
+              href="https://www.tiktok.com/@toxiscan4"
+              target="_blank"
+              rel="noopener"
+            >
+              <FaTiktok size={24} />
+            </IconButton>
+          </Box>
+        </Box>
       </Box>
     </ThemeProvider>
   );
